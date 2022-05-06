51a8c26174321b3aea9a9dd6ae82cbb397d9ab09
hosain-ghoraba <hussainghoraba@gmail.com>
2022-05-06 15:33:48 +0200

    let's start M2!

--- src/views/Questions.java
+++ src/views/Questions.java
@@ -6,29 +6,13 @@ public class Questions {
 	/* questions
     
     1- why override clone method ?
-    
-    abdo edit
+   
  
- */
-  int y = 7;
-  int z =6;
-  int f= 5;
-  int lala = 44;
-  //alloooo
-  //oonk
-  //egydwvyws
- 
- 
- 
- 
- 
+    */
+	
+	
+	
 }
- 
- 
-	
-	
-	
-	
 	
 	
 	
