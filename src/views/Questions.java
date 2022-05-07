9bd1cfe7031adcd3159482f8b5dc6287fc12bfed
Dell <Dell@192.168.1.13>
2022-05-07 19:09:43 +0200

    done doing the very basic logic of the apply method of the first 5 effects

--- src/views/Questions.java
+++ src/views/Questions.java
@@ -8,7 +8,13 @@ public class Questions {
     2- in Dodge class, in apply method, should I typecast result speed to int or round it to the closest integer?
     3- in Embrace class, in apply method, should I typecast result values (currentHp, Mana, speed,attackDamage) to int or round it to the closest integer?
     4- in powerUp class, (same question, typecast or round?)
-   
+    5- important : what to do if the object carry multiple effects that does a percentage change on the same value,
+                   and then one effect is removed, should the recovered percentage be calculated based on the 
+                   the current value ?
+    6 = can a champion carry multiple instances of the same Ability? ( this can happen if the champion was               
+        carrying multiple disarm effects
+        
+    7- I am stuck at remove method    
     */
 	
 	
