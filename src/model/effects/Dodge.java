7f572824d1228fc6bd64ea9356e0c32c78509172
Dell <Dell@192.168.1.13>
2022-05-07 19:46:25 +0200

    done doing the verybasic logic the first 5 effects ( apply method only)...(

--- src/model/effects/Dodge.java
+++ src/model/effects/Dodge.java
@@ -13,7 +13,7 @@ public class Dodge extends Effect {
 	@Override
 	public void apply(Champion c) throws CloneNotSupportedException {
 		//  50% chance to dodge : done in attake method 
-		// increase speed by 5 % : 
+		//    increase speed by 5 % : 
 		c.setSpeed((int) (c.getSpeed() * 1.05)); // should I do this(typecast to int) or should I round to the closeset integer?
 		Effect e = new Dodge(); // what is the duration ?
 		c.getAppliedEffects().add(e);
