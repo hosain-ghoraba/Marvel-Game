99e118f36620355d49b3c20df14247a86dd61174
Dell <Dell@192.168.1.10>
2022-05-30 23:46:42 +0200

    vv

--- src/views/gamewindow.java
+++ src/views/gamewindow.java
@@ -1,17 +1,49 @@
 package views;
+import model.world.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.concurrent.locks.Condition;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
+import engine.Game;
+import engine.Player;
+import model.world.Champion;
+import model.*;
 public class gamewindow extends JFrame {
-	
-	public gamewindow() {
+	JTextArea txtchamp ;
+	Player pl1 ;
+	Player pl2 ;
+	public gamewindow(Game game ,Player p1 , Player p2) {
 		this.setVisible(true);
+		this.pl1=pl1;
+		this.pl2=pl2 ;
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500,500);
 	//	this.setResizable(false);
 		ImageIcon logo = new ImageIcon("logo.png");
 		this.setIconImage(logo.getImage());
+		txtchamp = new JTextArea();
+		txtchamp.setPreferredSize(new Dimension(150, getHeight()));
+
+		txtchamp.setEditable(false);
+		txtchamp.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+		add(txtchamp, BorderLayout.NORTH);
+updatetxt();
+
+	}
+	//eee
+	
+public void updatetxt(){
+		String s = pl1.getName() +" : "+"\n" + "remaining champions :";
+		for(Champion x : pl1.getTeam()) {
+			if(x.getCondition()!=(model.world.Condition.KNOCKEDOUT)) {
+				s+= x.getName() + "  ";
+			}
+		}
 	}
 	
 }
