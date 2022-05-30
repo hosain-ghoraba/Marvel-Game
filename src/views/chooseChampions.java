788de48df5e096f4c4176ed40456f0c60a1eaec3
Dell <Dell@192.168.1.10>
2022-05-30 13:25:04 +0200

    choosechampsdone

--- src/views/chooseChampions.java
+++ src/views/chooseChampions.java
@@ -2,6 +2,7 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +11,9 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import engine.Game;
 import engine.Player;
@@ -20,6 +23,7 @@ public class chooseChampions extends JFrame  implements ActionListener{
 	private ArrayList<JButton> champsbuttons;
 private Player pl1 ;
 private Player pl2 ;
+private JTextArea txtchamp;
 
 	public chooseChampions (Game game , Player pl1, Player pl2)
 	{
@@ -32,10 +36,26 @@ private Player pl2 ;
 	//	this.setResizable(false);
 		ImageIcon logo = new ImageIcon("logo.png");
 		this.setIconImage(logo.getImage());
+		txtchamp = new JTextArea();
+		txtchamp.setPreferredSize(new Dimension(150, getHeight()));
+
+		txtchamp.setEditable(false);
+		txtchamp.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+		add(txtchamp, BorderLayout.EAST);
+updatetxt();
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		JPanel champs = new JPanel();
 		// set it to use the GridLayout with 3 columns in width
-		champs.setSize(new Dimension(400,400));
-		champs.setLayout(new GridLayout(0, 4));
+		champs.setSize(new Dimension(350,500));
+		champs.setLayout(new GridLayout(0, 3));
 		// add it in the center of the JFrame
 		champsbuttons = new ArrayList<>() ;
 		this.add(champs, BorderLayout.CENTER);
@@ -53,6 +73,8 @@ private Player pl2 ;
 
 			// and also add it to the ArrayList for later use
 			champsbuttons.add(btnchamp);	}
+
+		JOptionPane.showMessageDialog(null, pl1.getName()+ " : choose your 3 champions","", JOptionPane.INFORMATION_MESSAGE);
 
 		this.validate();;
 		this.repaint();
@@ -72,15 +94,20 @@ private Player pl2 ;
 	}
 
 	@Override
+	
+	
 	public void actionPerformed(ActionEvent e) {
 JButton x = 	(JButton)e.getSource() ;
 int i = champsbuttons.indexOf(x);
 if(pl1.getTeam().size()<3) {
 pl1.getTeam().add(Game.getAvailableChampions().get(i));
+updatetxt();
+x.setEnabled(false) ;
 
 
 if(pl1.getTeam().size()==3) {
 	this.setTitle(pl2.getName()+"   choose your champions");
+	JOptionPane.showMessageDialog(null, pl2.getName()+ " : choose your 3 champions","", JOptionPane.INFORMATION_MESSAGE);
 
 }
 }
@@ -94,7 +121,11 @@ if(pl1.getTeam().size()==3) {
 
 		}
 		pl2.getTeam().add(Game.getAvailableChampions().get(i));
-   if(pl2.getTeam().size()==3) {
+		x.setEnabled(false) ;
+
+		updatetxt();
+
+		if(pl2.getTeam().size()==3) {
 		this.dispose();
 
 	   new gamewindow() ;
@@ -103,4 +134,30 @@ if(pl1.getTeam().size()==3) {
 	
 	}
 
+	public void  updatetxt () {
+		String s = pl1.getName()+ "'s"+ " champions :"+ "\n" ;
+		for(int i = 0 ; i< pl1.getTeam().size() ;i++) {
+			s+= pl1.getTeam().get(i).getName() + "\n" ;
+			
+		}
+		String v = pl2.getName()+ "'s"+ " champions :"+ "\n" ;
+		for(int i = 0 ; i< pl2.getTeam().size() ;i++) {
+			v+= pl2.getTeam().get(i).getName() + "\n" ;
+			
+		}
+		
+		txtchamp.setText(s+ "\n"+v);
+
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
