51def66b4dfb4cbccb57e435906af8d68d2d1557
Ahmed Yousry <ahmedyousry@gmail.com>
2022-06-01 23:42:23 +0200

    ll

--- src/views/marvelcontroler.java
+++ src/views/marvelcontroler.java
@@ -3,11 +3,42 @@ package views;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+
+import engine.Game;
 import engine.Player;
 
 public class marvelcontroler implements ActionListener {
 
-	
+	public marvelcontroler (Game game , Player pl1 , Player pl2)
+	{
+		JButton zero_zero = new JButton ("1");
+		JButton zero_one = new JButton ("2");
+		JButton zero_two = new JButton ("3");
+		JButton zero_three = new JButton ("4");
+		JButton zero_four = new JButton ("5");
+		JButton one_zero = new JButton ();
+		JButton one_one = new JButton ();
+		JButton one_two = new JButton ();
+		JButton one_three = new JButton ();
+		JButton one_four = new JButton ();
+		JButton two_zero = new JButton ();
+		JButton two_one = new JButton ();
+		JButton two_two = new JButton ();
+		JButton two_three = new JButton ();
+		JButton two_four = new JButton ();
+		JButton three_zero = new JButton() ;
+		JButton three_one =new JButton() ;
+		JButton three_two = new JButton() ;
+		JButton three_three = new JButton();
+		JButton three_four = new JButton();		
+		JButton four_zero = new JButton() ;
+		JButton four_one =new JButton() ;
+		JButton four_two = new JButton() ;
+		JButton four_three = new JButton();
+		JButton four_four = new JButton();
+
+	}
 	 
 	public static void main(String[] args) {
 
