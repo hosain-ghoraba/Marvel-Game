935bc8a9316fc00efbd56c390f73fb2ebe80d5bf
Dell <Dell@192.168.1.13>
2022-06-03 05:46:46 +0200

    as

--- src/views/loginWindow.java
+++ src/views/loginWindow.java
@@ -72,7 +72,7 @@ public class loginWindow extends JFrame implements ActionListener  {
 	    
 	    
 	    
-	    JButton Enter = new JButton("Play");
+	    JButton Enter = new JButton("Play");	   
 	    Enter.addActionListener(this);
 	    Enter.setBounds(302,430, 150,40);
 	    Enter.setFont(new Font ("Times New Roman",Font.BOLD,17));
