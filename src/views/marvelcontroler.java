978496c3dcb54999af53ba915b4e74ba6c562efb
Dell <Dell@192.168.1.13>
2022-06-05 16:31:53 +0200

    kuhk

--- src/views/marvelcontroler.java
+++ src/views/marvelcontroler.java
@@ -504,7 +504,7 @@ default: break ;
 		// TODO Auto-generated method stub
 		
 	}
-public static void main(String[] args) {
+   public static void main(String[] args) {
 //	while(true) {
 	String[] options = new String[] {"Right", "left", "up"};
 	ArrayList<String> o = new ArrayList<>() ;
