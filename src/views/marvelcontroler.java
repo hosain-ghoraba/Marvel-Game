97064e435f8315b49db0841089619ae4ceee6292
Dell <Dell@192.168.1.13>
2022-06-09 07:29:03 +0200

    جاهز للتسليم إن شاء الله :) (يا رب)

--- src/views/marvelcontroler.java
+++ src/views/marvelcontroler.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
